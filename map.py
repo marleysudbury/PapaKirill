817c85f45c68a350e5e7d2dad7e39a88926fd0fe
Austinae <austin.william.ga@gmail.com>
2018-10-22 13:32:42 +0100

    New Map Version

--- map.py
+++ map.py
@@ -2,74 +2,101 @@ from items import *
 from evidence import *
 from characters import *
 
-room_reception = {
-    "name": "Reception",
+room_pizzeria = [{
+    "name": "Papa Kirill's",
 
-    "description":
-    """You are in a maze of twisty little passages, all alike. Next to you is the School of Computer Science and Informatics reception. The receptionist, Matt Strangis, seems to be playing an old school text-based adventure game on his computer. There are corridors leading to the south and east. The exit is to the west. There is some dough on the side counter, hmm.""",
+    "description": """You, Detective Kirill Sidirov, are standing in the finest
+    pizzeria in Chicago: Papa Kirill's. You are very familiar with the
+    ins and outs of this eatery because you have spent most of your
+    childhood in it with your uncle, running around, knocking things over, 
+    eating free pizza. You start your detective work by searching the
+    building in the hope of finding any clues that might explain your
+    uncle's mysterious death. 
 
-    "exits": {"south": "Admins", "east": "Tutor", "west": "Parking"},
+    Firstly, you examine the body:
 
-    "items": [item_biscuits, item_handbook],
+    Under a cooking apron, your uncle is wearing a blood-stained white
+    tuxedo with a black bow tie. Six stab wounds are on his back. 
+    Beside the corpse is a table. On this table sits Papa Kirill’s chef’s hat.
+    Curiously, there is still dough on the table and there is a hand
+    mark in the middle of it, that’s a big hand!
+    You should take a picture of any evidence.
 
-    "people": [person_barman], #added barman so i can test in reception, he will be moved later on
+    After examining your uncle's corps you get a sense that this murder
+    was very personal. Your uncle might not be the virtuous innocent
+    man you thought he was. 
 
-    "evidence": [evidence_dough]
-}
+    Under the table, you notice the edge of a purple piece of paper.
 
-room_admins = {
-    "name": "MJ and Simon's room",
+    The evidence left in the restaurant was very minor, you need to look
+    elsewhere. The Car Park and Delivery Station is to the west and the 
+    Alleyway is to the east. You might need to check them both out to really
+    get a sense of what went wrong the night of the 21rst of July 1956.
+""",
 
-    "description":
-    """You are leaning agains the door of the systems managers' room. Inside you notice Matt "MJ" John and Simon Jones. They ignore you. To the north is the reception.""",
+    "exits": {"west": "Parking", "east": "Alleyway", "down": "Sewers"},
 
-    "exits":  {"north": "Reception"},
+    "items": [item_flyer],
+
+    "evidence": [evidence_dough],
+
+    "characters": [],
+
+},
+    {
+    "name": "Papa Kirill's",
+
+    "description": """""",
+
+    "exits": {"west": "Parking", "east": "Alleyway", "down": "Sewers"},
 
     "items": [],
 
-    "evidence": []
-}
+    "evidence": [],
 
-room_tutor = {
-    "name": "your personal tutor's office",
+    "characters": [character_fakecop],
+    }
+]
+room_parking = [{
+    "name": "Car Park and Delivery Station",
 
-    "description":
-    """You are in your personal tutor's office. He intently stares at his huge monitor, ignoring you completely. On the desk you notice a cup of coffee and an empty pack of biscuits. The reception is to the west.""",
+    "description": """You are in the Car Park and Delivery Station. Here is where
+    customers would park their cars to eat in the restaurant. This is also
+    where delivery bikes would leave to bring food directly to the
+    customers' houses. You notice a big tire mark left on the concrete
+    road. No delivery bike could make such an imprint. You can tell it came
+    from a car but which one remains a mystery. Hmm, perhaps we could ask
+    if any locals saw a car speeding up on the main road leading up to
+    the pizzeria.
+    
+    You look around but find no other clues. To the south is a bar. To the
+    east is the restaurant.""",
 
-    "exits": {"west": "Reception"},
+    "exits": {"west": "Parking", "east": "Alleyway", "down": "Sewers"},
+
+    "items": [item_flyer],
+
+    "evidence": [evidence_dough],
+
+    "characters": [],
+
+},
+    {
+    
+    "name": "Car Park and Delivery Station",
+    
+    "description": """""",
+
+    "exits": {"west": "Parking", "east": "Alleyway", "down": "Sewers"},
 
     "items": [],
 
-    "evidence": []
-}
+    "evidence": [],
 
-room_parking = {
-    "name": "the parking lot",
+    "characters": [character_fakecop],
+    }
+]
 
-    "description":
-    """You are standing in the Queen's Buildings parking lot. You can go south to the COMSC reception, or east to the general office.""",
-
-    "exits": {"east": "Office", "south": "Reception"},
-
-    "items": [],
-
-    "evidence": []
-}
-
-room_office = {
-    "name": "the general office",
-
-    "description":
-    """You are standing next to the cashier's till at 30-36 Newport Road. The cashier looks at you with hope in their eyes. If you go west you can return to the Queen's Buildings.""",
-
-    "exits": {"west": "Parking"},
-
-    "people": [person_cop],
-
-    "items": [item_pen, item_revolver],
-
-    "evidence": []
-}
 
 rooms = {
     "Reception": room_reception,
