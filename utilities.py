5f469a50ab6909fef53d0bc5f07c9a3396eaaf72
Marley Sudbury <marleysudbury@gmail.com>
2018-10-23 19:56:51 +0100

    Room descriptions wrap correctly

--- utilities.py
+++ utilities.py
@@ -67,10 +67,43 @@ def print_description(room):
     minimap_lines = generate_minimap(room['exits'], has_a_weapon)
 
     line_counter = 0
-    line_without_minimap = line_length - len(minimap_lines[line_counter])
+    # line_without_minimap = line_length - len(minimap_lines[line_counter])
 
-    char_counter = 0
-    description_line = ''
+    word_counter = 0
+    new_desc = description.split(' ')
+    two_words = [False, False]
+    while word_counter < len(new_desc) - 1:
+        new_line = ""
+        if two_words[1]:
+            new_line += "\n"
+            new_line += two_words[1]
+            new_line += " "
+            two_words = [False, False]
+        if line_counter < len(minimap_lines):
+            while len(new_line) + len(new_desc[word_counter]) + len(minimap_lines[line_counter]) + 1 < line_length:
+                new_line += new_desc[word_counter]
+                new_line += " "
+                word_counter += 1
+            while len(new_line) + len(minimap_lines[line_counter]) < line_length:
+                new_line += " "
+            new_line += minimap_lines[line_counter]
+        else:
+            while (word_counter < len(new_desc) -1) and len(new_line) + len(new_desc[word_counter]) + 1 < line_length:
+                if "\n\n" in new_desc[word_counter]:
+                    two_words = new_desc[word_counter].split("\n\n")
+                    print(two_words)
+                    new_line += two_words[0]
+                    word_counter += 1
+                    # print("BREAKING")
+                    break
+                new_line += new_desc[word_counter]
+                new_line += " "
+                word_counter += 1
+
+        print(new_line)
+        line_counter += 1
+
+    """        
     for char in description:
         if char == "\n":
             char_counter = 0
@@ -94,3 +127,4 @@ def print_description(room):
                 description_line += char
                 description_line += '\n'
     print(description_line)
+    """
