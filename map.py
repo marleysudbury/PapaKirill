21d3c5645577a8af5f823c5aeb8a8ce0f6bba140
Veski0 <j.bateman@techie.com>
2018-10-23 21:29:45 +0100

    Updated map descriptions.

--- map.py
+++ map.py
@@ -6,17 +6,17 @@ from characters import *
 room_pizzeria = [{
     "name": "Papa Kirill's",
 
-    "description": """You, Detective Kirill Sidirov, are standing in the finest pizzeria in Chicago: Papa Kirill's. You are very familiar with the ins and outs of this eatery because you have spent most of your childhood init with your uncle, running around, knocking things over, eating free pizza. You start your detective work by searching the building in the hope of finding any clues that might explain your uncle's mysterious death.
+    "description": """You, Detective Kirill Sidirov, are standing in the finest pizzeria in Chicago: Papa Kirill's. Having spent most of your childhood here with your uncle running around, knocking things over and eating free pizza, you are very familiar with the ins and outs of this eatery. You start your detective work by searching the building in the hope of finding any clues that might explain your uncle's mysterious death.
 
-Firstly, you examine the body:
+Stooping down, you examine the body:
 
-Under a cooking apron, your uncle is wearing a blood-stained white tuxedo with a black bow tie. Six stab wounds are on his back. Beside the corpse is a table. On this table sits Papa Kirill’s chef’s hat. Curiously, there is still dough on the table and there is a hand mark in the middle of it, that’s a big hand! You should inspect it.
+Under a stained cooking apron lies your uncle, face down. He is dead. Garbed in a blood-stained white tuxedo with a black bow tie, six stab wounds line his back. Beside the corpse is a table. On this table sits Papa Kirill’s chef’s hat. Curiously, there is still dough on the table and there is a hand mark in the middle of it, that’s a big hand! You should inspect it.
 
-After examining your uncle's corpse you get a sense that this murder was very personal. Your uncle might not be the virtuous innocent man you thought he was. 
+This murder seems very personal.. Who would do this and why.. Your uncle might not be the virtuous innocent man you once thought he was..
 
 Under the table, you notice the edge of a purple piece of paper.
 
-The evidence left in the restaurant was very minor, you need to look elsewhere. The Car Park and Delivery Station is to the west and the Alleyway is to the east. You might need to check them both out to really get a sense of what went wrong the night of the 21st of July 1956.""",
+The evidence here is sparse, you need to look elsewhere. The Car Park and Delivery Station is west and the Alleyway is east.""",
 
     "exits": {"west": "Car Park and Delivery Station", "east": "Alleyway"},
 
@@ -73,7 +73,7 @@ The evidence left in the restaurant was very minor, you need to look elsewhere. 
 room_parking = [{
     "name": "Car Park and Delivery Station",
 
-    "description": """You are in the Car Park and Delivery Station. Here is where customers would park their cars to eat in the restaurant. This is also where delivery bikes would leave to bring food directly to the customers' houses. You notice a big tire mark left on the concrete road. No delivery bike could make such an imprint. You can tell it came from a car but which one remains a mystery. I should ask Christine.
+    "description": """You are in the Car Park and Delivery Station. There are a few cars scattered about the lot, presumably customers. A large mark, likely left by a tyre, heads off down the concrete road. No delivery bike could make such an imprint - it was definitely a car but which one remains a mystery. I should ask Christine.
 
 Oh, there's a bullet on the floor.
 
--- utilities.py
+++ utilities.py
@@ -91,10 +91,8 @@ def print_description(room):
             while (word_counter < len(new_desc) -1) and len(new_line) + len(new_desc[word_counter]) + 1 < line_length:
                 if "\n\n" in new_desc[word_counter]:
                     two_words = new_desc[word_counter].split("\n\n")
-                    # print(two_words)
                     new_line += two_words[0]
                     word_counter += 1
-                    # print("BREAKING")
                     break
                 new_line += new_desc[word_counter]
                 new_line += " "
@@ -102,29 +100,3 @@ def print_description(room):
 
         print(new_line)
         line_counter += 1
-
-    """        
-    for char in description:
-        if char == "\n":
-            char_counter = 0
-        if line_counter < len(minimap_lines):
-            if char_counter < line_without_minimap:
-                char_counter += 1
-                description_line += char
-            else:
-                char_counter = 0
-                description_line += char
-                description_line += minimap_lines[line_counter] + '\n'
-                line_without_minimap = line_length - len(minimap_lines[line_counter])
-                line_counter += 1
-        else:
-            if char_counter < line_length:
-                char_counter += 1
-                description_line += char
-            else:
-                char_counter = 0
-                line_counter += 1
-                description_line += char
-                description_line += '\n'
-    print(description_line)
-    """
