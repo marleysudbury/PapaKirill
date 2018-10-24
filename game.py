238f67d81b5fa4a52e3883935f6b503053124dbc
Veski0 <j.bateman@techie.com>
2018-10-25 00:07:23 +0100

    Almost there.

--- game.py
+++ game.py
@@ -349,13 +349,14 @@ first_done = True
 def demo(room_ver):
     if room_ver == 0 and player.current_room == map.rooms["Car Park and Delivery Station"] and len(player.current_room["rooms"][room_ver]['evidence']) == 0:
         map.rooms["Car Park and Delivery Station"]["version"] = 1
+    if room_ver == 0 and player.current_room == map.rooms["Sewers"] and len(player.current_room["rooms"][room_ver]['evidence']) == 0:
+        map.rooms["Sewers"]["version"] = 1
     global first_done
     if first_done and room_ver == 0 and player.current_room == map.rooms["Papa Kirill's"] and len(player.current_room["rooms"][room_ver]['evidence']) == 0:
         map.rooms["Papa Kirill's"]["version"] = 1
         first_done = False
     if room_ver == 0 and player.current_room == map.rooms["Andy's Jazz Club"] and len(player.current_room["rooms"][room_ver]['characters']) == 0:
         map.rooms["Andy's Jazz Club"]["version"] = 1
-    if room_ver == 1 and player.current_room == map.rooms["Andy's Jazz Club"] and len(player.current_room["rooms"][room_ver]['evidence']) == 0:
         map.rooms["Alleyway"]["version"] = 1
     if room_ver == 1 and player.current_room == map.rooms["Alleyway"] and len(player.current_room["rooms"][room_ver]['evidence']) == 0:
         map.rooms["Alleyway"]["version"] = 2
